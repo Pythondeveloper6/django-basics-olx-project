2108fccaf39170c5cb876fc814d155312a61491e
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-06-03 21:19:29 +0200

    add ad models v1

--- ad/models.py
+++ ad/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Ad(models.Model):
-    code = models.CharField(max_length=12)
+    code = models.CharField(max_length=12,blank=True, null=True)
     owner = models.ForeignKey(User,related_name='ad_owner', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now=True)
@@ -13,6 +13,19 @@ class Ad(models.Model):
     content = models.TextField(max_length=1000)
     price = models.IntegerField(default=1)
     category = models.ForeignKey('Category', related_name='ad_category', on_delete=models.CASCADE)
+
+    ### relation : main category 
+
+    def save(self, *args, **kwargs): 
+        ## logic
+        self.code = '##'+((str(self.id)).center(10,'0'))
+        super(Ad, self).save(*args, **kwargs) 
+
+
+    def __str__(self):
+        return self.name
+
+
 
 
 
@@ -26,6 +39,7 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     main_category = models.ForeignKey('self', related_name='maincategory', on_delete=models.CASCADE , blank=True, null=True)
 
+    ## relation sub category 
 
     def __str__(self):
         return self.name
