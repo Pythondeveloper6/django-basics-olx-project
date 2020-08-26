2ffd7f57cd7da822c90aaf2f3df21b884e966150
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-08-26 22:55:53 +0200

    add the paypal & contact form

--- contact/views.py
+++ contact/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
-
+from django.conf import settings
 # Create your views here.
 
 
 
 def sendemail(request):
-    send_mail(
-    'Subject here',
-    'Here is the message.',
-    'from@example.com',
-    ['to@example.com'],
-    fail_silently=False,
-)
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        
+        send_mail(
+        f'from {email} - subject {subject}',
+        f'message from {email} : message {message}',
+        email,
+        [settings.EMAIL_HOST_USER],
+        fail_silently=False,
+    )
 
 
     return render(request,'contact/contact.html',{})
--- project/settings.py
+++ project/settings.py
@@ -150,12 +150,12 @@ LOGIN_REDIRECT_URL = '/accounts/profile'
 
 
 
-EMAIL_BACKEND = 
-EMAIL_HOST =
-EMAIL_HOST_USER=
-EMAIL_HOST_PASSWORD=
-EMAIL_PORT=
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER= 'pythondeveloper6@gmail.com'
+EMAIL_HOST_PASSWORD= 'fmuidogiebnraekg'
+EMAIL_PORT= '587'
+EMAIL_USE_TLS = True
 
 
 #### deploy
